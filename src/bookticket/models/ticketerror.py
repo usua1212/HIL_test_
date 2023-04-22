7e7ad96a7ef91b5eb091631725ff8fdf98a7f6cf
StasOstapenko <36300697+usua1212@users.noreply.github.com>
2023-04-22 12:37:50 +0300

    2

--- src/bookticket/models/ticketerror.py
+++ src/bookticket/models/ticketerror.py
@@ -1,23 +1,7 @@
 
+
 class TicketError(Exception):
     def __init__(self, message):
         self.message = message
-
-try:
-    if not self.routes[route_id].departure_time > current_date:
-        raise TicketError("Sorry, there are no seats available on this bus")
-try:
-    if not avail_seats:
-        raise TicketError("Sorry, there are no seats available on this bus")
-try:
-    if seat_num in booked_seats:
-        raise TicketError("Sorry, this seat has already been booked")
-
-
-
-
-
-
-
-
+        super().__init__(self.message)
 
